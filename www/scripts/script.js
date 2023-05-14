2f9ef53620cc01b80856141e433efcf5f8b31de0
tigers-jaw <light.springs.eternal@gmail.com>
2023-05-14 19:14:53 +0300

    slider

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 
+  //reviews
 
+  $('.js-slider').slick({
+    centerMode: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnFocus: true,
+    pauseOnHover: true,
 
-
-
-
+  });
 
 });
